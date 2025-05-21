4e45f09029a039ec14c2daee2966eee36fad7331
carloszuluaga-20 <cazuluaga-2024a@corhuila.edu.co>
2025-05-20 19:40:36 -0500

    Proyecto final

--- Ejercicios-estructura/src/Ejercicio1/java/Figura.java
+++
@@ -1,9 +0,0 @@
-package Ejercicio1.java;
-
-public abstract class Figura {
-    public abstract double calcularArea();
-
-    public void mostrarArea() {
-        System.out.println("El área es: " + calcularArea());
-    }
-}
